e6d45a35dcafc5225fe10c999376dfe0bf6cd248
Daniel Castellano <daniel.castellanom@gmail.com>
2017-10-12 21:28:03 -0600

    DDL y DML, con sub menus

--- pgadmin.py
+++ pgadmin.py
@@ -18,6 +18,7 @@ class DatabaseAction():
 
         self.validaCreacion(createDBQuery)
 
+    """AQUI INICIAN LAS FUNCIONES DDL"""
     def create_table(self):
         nombreTabla = input("Ingrese el nombre de la tabla: ")
         atributosTabla = input("Los atributos se deben ingresar de la siguiente manera:\n"
@@ -62,12 +63,76 @@ class DatabaseAction():
             print("Escriba una respuesta correcta")
             self.validaCreacion(query)
 
-mainMenu = menu.Menu()
+    """AQUI INICIAN LAS FUNCIONES DDL"""
 
+    def insert_data(self):
+        nombreTabla = input("Ingrese el nombre de la tabla a la que desea insertar datos: ")
+        nombreColumnas = input("Ingrese el nombre de las columnas: ")
+        valores = input("Ingrese los valores: ")
+
+        insertQuery = """INSERT INTO {} ({}) VALUES ({})""".format(nombreTabla, nombreColumnas,valores)
+
+        self.validaCreacion(insertQuery)
+
+    def delete_data(self):
+        nombreTabla = input("Ingrese el nombre de la tabla de la cual desea borrar datos: ")
+        condicion = input("Ingrese la condición que desea evaluar para borrar los datos: ")
+
+        deleteQuery = """DELETE FROM {} WHERE {}""".format(nombreTabla,condicion)
+
+        self.validaCreacion(deleteQuery)
+
+    def update_data_where(self):
+        nombreTabla = input("Ingrese el nombre de la tabla de la cual desea actualizar los datos: ")
+        columna = input("Ingrese la columna que desea actualizar: ")
+        condicion =  input("Ingrese la condición que sea evaluar para actualizar los datos: ")
+
+        updateWhereQuery = """UPDATE {} SET {} WHERE {}""".format(nombreTabla,columna,condicion)
+
+        self.validaCreacion(updateWhereQuery)
+
+    def update_data_general(self):
+        nombreTabla = input("Ingrese el nombre de la tabla de la cual desea actualizar los datos: ")
+        condicion = input("Ingrese la condicion que desea ejecutar para la actualización: \n"
+                          "Ejemplo de condición: SET price = price * 1.10 \n")
+
+        updateGeneralQuery = """UPDATE {} SET {}""".format(nombreTabla,condicion)
+
+        self.validaCreacion(updateGeneralQuery)
+
+    def listaFunciones(self):
+        lista = ["Create Table","Alter Table","Delete Table"]
+        print("Hola")
+
+dbaction = DatabaseAction()
+
+mainMenu = menu.Menu()
+ddlMenu = menu.Menu()
+ddlMenu.set_title("Menu DDL")
+ddlMenu.set_options([
+    ("Crear una nueva tabla",dbaction.create_table),
+    ("Eliminar una tabla existente",dbaction.delete_table),
+    ("Alterar una tabla existente",dbaction.alter_table),
+    ("Volver al menú principal",ddlMenu.close)])
+
+dmlMenu = menu.Menu()
+dmlMenu.set_title("Menu DML")
+dmlMenu.set_options([
+    ("Insertar datos",dbaction.insert_data),
+    ("Borrar datos",dbaction.delete_data),
+    ("Actualizar datos",dbaction.update_data_where),
+    ("Volver al menú principal",dmlMenu.close)])
+
+
+mainMenu.set_title("Bienvenido a Py Admin")
 mainMenu.set_options([
-("Conectarse a una base de datos", db.conn)])
+    ("Crear una base de datos", dbaction.create_database),
+    ("Menu DDL", ddlMenu.open),
+    ("Menu DML",dmlMenu.open),
+    ("Ver lista de funciones",dbaction.listaFunciones)])
 
 
 if __name__ == '__main__':
     mainMenu.open()
+
 
